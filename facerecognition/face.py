06ecac7f3d75497001ec811ed73c64b9e1636942
ChrisB85 <krzysztof.blachowicz@gmail.com>
2025-05-18 18:30:49 +0200

    Refactor crop function in face recognition module to ensure all coordinates are converted to integers, improving consistency and preventing potential errors during image cropping.

--- facerecognition/face.py
+++ facerecognition/face.py
@@ -27,10 +27,16 @@ def crop(image, x, y, w, h):
     to an image with the same aspect ratio as the face training data.  Might
     return a smaller crop if the box is near the edge of the image.
     """
+    # Convert all coordinates to integers
+    x = int(x)
+    y = int(y)
+    w = int(w)
+    h = int(h)
+    
     crop_height = int((config.FACE_HEIGHT / float(config.FACE_WIDTH)) * w)
     midy = y + h / 2
-    y1 = max(0, midy - crop_height / 2)
-    y2 = min(image.shape[0] - 1, midy + crop_height / 2)
+    y1 = int(max(0, midy - crop_height / 2))
+    y2 = int(min(image.shape[0] - 1, midy + crop_height / 2))
     return image[y1:y2, x:x + w]
 
 
